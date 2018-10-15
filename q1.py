729daf28b3b26cbb87b0769b9960ef195d305d7e
Zac Keskin <zackeskin@outlook.com>
2018-10-15 19:17:01 +0100

    First attempt at q1

--- q1.py
+++ q1.py
@@ -4,6 +4,17 @@ import numpy as np
 
 # modify this function, and create other functions below as you wish
 def question01(portfolios):
-  # modify and then return the variable below
-  answer = 0
-  return answer
+
+    N = len(portfolios)
+    combinations = np.zeros(N*(N-1))
+
+    ## Loop over all combinations
+    i = 0
+    for p in portfolios:
+        for q in portfolios:
+            if p != q:
+                i+=1
+                combinations[i] = p ^ q
+
+    answer = max(combinations)
+    return answer
