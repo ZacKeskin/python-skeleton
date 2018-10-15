c4fbbbc2adc1c6546e8e07e9089a5b1c12958132
Zac Keskin <zackeskin@outlook.com>
2018-10-15 23:00:38 +0100

    First attempt at q4

--- q4.py
+++ q4.py
@@ -2,9 +2,21 @@
 
 import numpy as np
 
-#compute all combinations for two portfolios
-def question04(rows, numberMachines):
-  # modify and then return the variable below
-  answer = -1
-  return answer
 
+def question04(rows, numberMachines):
+
+    ## Convert to ndarray and replace str(X) with int(0)    
+    rows = np.array(rows)
+    rows[rows=='X']=0
+
+    ## Sort
+    rows = np.sort(a=rows.astype(int),kind='heapsort')
+    
+    ## Rows with N contiguous 'X's
+    new_rows = rows[rows[:,numberMachines-1]==0]
+
+    ## Minimum row
+    try:
+        return np.min(new_rows[:,numberMachines:].sum(axis=1))
+    except:
+        return 0
